813eb1a4aa54c18b442ae18312b9759fbc2c93c7
pbensi.contact@gmail.com <pbensi.contact@gmail.com>
2025-12-09 08:50:59 +0800

    im tired-9-17

--- js/components/contact.js
+++ js/components/contact.js
@@ -162,7 +162,7 @@ class Contact {
                 redirect: "follow",
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "text/plain;charset=utf-8"
                 },
                 body: JSON.stringify(formData)
             });
